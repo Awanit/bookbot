dcfc2b13d15ce8030aad883c6a364abea1c53922
Awanit <amirkhanyangarik@gmail.com>
2024-10-15 17:13:40 +0500

    added file word count

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+def words_count(text):
+    words = text.split()
+    return len(words)
+
 def main():
     file_path = "books/frankenstein.txt"
 
     with open(file_path) as f:
         file_contents = f.read()
         print(file_contents)
+        count = words_count(file_contents)
+        print(count)
 if __name__ == "__main__":
     main()
