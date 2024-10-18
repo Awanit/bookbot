e7b9c9b6476c54e3c50a0fc0131dac894fd20e16
Awanit <amirkhanyangarik@gmail.com>
2024-10-18 15:54:09 +0500

    added funcion to count unique leters

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def char_count(text):
+    char_dict = {}
+    lower_text = text.lower()
+    
+    for char in lower_text:
+        if char.isalpha():  # Count only letters
+            # Use dict.get() to fetch the current count or default to 0
+            char_dict[char] = char_dict.get(char, 0) + 1
+    
+    return char_dict
+        
 def words_count(text):
     words = text.split()
     return len(words)
@@ -8,7 +19,9 @@ def main():
     with open(file_path) as f:
         file_contents = f.read()
         print(file_contents)
-        count = words_count(file_contents)
-        print(count)
+        w_count = words_count(file_contents)
+        print(w_count)
+        c_count = char_count(file_contents)
+        print(c_count)
 if __name__ == "__main__":
     main()
